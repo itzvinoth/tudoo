33dc9c0dffd82dbee71970c386c5ad27371df54a
mevinoth <vinoth.vinothrenga@gmail.com>
2021-10-30 22:25:18 +0530

    misc changes

--- tudoo-front/src/views/About.vue
+++ tudoo-front/src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an scheduled tasks page</h1>
+    <h1>This is an scheduled tasks page</h1>Go to <a href="/tasks">Tasks</a> page.
     <p>Project <a href="https://www.taskade.com/">taskade</a> is little complex to use.</p>
   </div>
 </template>
