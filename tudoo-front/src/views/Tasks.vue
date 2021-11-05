6eafd338fb4e366f40cf29e6da3cae189be64e19
mevinoth <vinoth.vinothrenga@gmail.com>
2021-11-05 17:16:47 +0530

    buggy code pushed

--- tudoo-front/src/views/Tasks.vue
+++ tudoo-front/src/views/Tasks.vue
@@ -1,23 +1,6 @@
 <template>
 	<div class="tasks">
 		<h1>This is an tasks page</h1>
-		<ul>
-			<li v-for="(value, index) in contents" :key="index" class="">
-				<div class="wrapper">
-					<!-- <span class="draggable-handle">hai</span> -->
-					<div
-						:id="`content-${index}`"
-						class="content"
-						contenteditable
-						spellcheck="false"
-						@input="event => onInput(event, index)"
-						@keyup.delete="onRemove(index)"
-						@keydown.enter="onEnter"
-						@keydown.enter.prevent
-					/>
-				</div>
-			</li>
-		</ul>
 		<draggable
 			:list="contents"
 			:disabled="!enabled"
@@ -30,7 +13,16 @@
 		>
 			<template #item="{ element }">
 				<div class="list-group-item" :class="{ 'not-draggable': !enabled }">
-					{{ element.value }}
+					<div
+						:id="`content-${element.index}`"
+						class="content"
+						contenteditable
+						spellcheck="false"
+						@input="event => onInput(event, element.index)"
+						@keyup.delete="onRemove(element.index)"
+						@keydown.enter="onEnter"
+						@keydown.enter.prevent
+					/>
 				</div>
 			</template>
 		</draggable>
@@ -49,22 +41,22 @@ export default {
 		return {
 			enabled: true,
 			contents: [
-				{ value: 'Next adding drag and drop' },
-				{ value: 'Alignment needs to be fixed' },
-				{ value: 'CSS has to be updated' },
-				{ value: '' },
+				{ value: 'Next adding drag and drop', index: 0 },
+				{ value: 'Alignment needs to be fixed', index: 1 },
+				{ value: 'CSS has to be updated', index: 2 },
+				{ value: '', index: 3 },
 			]
 		}
 	},
-	created () {
-		const plugin = document.createElement('script')
-		plugin.setAttribute(
-			'src',
-			'../assets/js/dnd.js'
-		)
-		plugin.async = true
-		document.head.appendChild(plugin)
-	},
+	// created () {
+	// 	const plugin = document.createElement('script')
+	// 	plugin.setAttribute(
+	// 		'src',
+	// 		'../assets/js/dnd.js'
+	// 	)
+	// 	plugin.async = true
+	// 	document.head.appendChild(plugin)
+	// },
 	mounted () {
 		this.updateContents()
 	},
@@ -72,11 +64,13 @@ export default {
 		onInput (event, index) {
 			const value = event.target.innerText
 			this.contents[index].value = value
+			this.contents[index].index = this.contents.length - 1
+			console.log('contents: ', this.contents)
 		},
 		onEnter (event) {
-			// console.log('index: ', event.target.innerText)
+			console.log('index: ', event.target.innerText)
 			if (event.target.innerText) {
-				this.contents.push({value: ''})
+				this.contents.push({value: '', index: this.contents.length - 1})
 				this.$nextTick(() => {
 					let totalContents = this.contents.length - 1
 					let el = document.querySelector('#content-'+(totalContents))
